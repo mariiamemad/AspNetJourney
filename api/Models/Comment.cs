26742a225a97b3f0f3dc678351b99becf669f848
Mariam Emad <118808072+mariiamemad@users.noreply.github.com>
2025-01-26 11:56:00 +0200

    feat: comments, stock created

---
+++ api/Models/Comment.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Controllers;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int? StockID { get; set; }
+        public Stock? stock { get; set; }
+        public int? Id { get; set; }
+        public string Title { get; set; }= string.Empty;
+        public string Content { get; set; }=string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; }= string.Empty;
+    }
+}
